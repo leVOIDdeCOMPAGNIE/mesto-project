3a268975a3835fbb0df6d569cd02e59ea908451e
✨ <ultravoid@icloud.com>
2022-07-20 20:02:05 +0200

    hotfix/2ondReview

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-HTML5, CSS3, JS;
-OOP
+# Проект: Место
+
+## Проектная работа №3 для Яндекс.Практикум
+
+**Описание**
+
+1. Выполнена адаптивная верстка макету из Figma под разрашения: 1280, 320.
+Flexbox, Grid
+
+2. Выполнен интерактив на JS:
+- изменение данных профиля;
+- добавление новых карточек;
+- удаление карточек;
+- увеличение фото;
+- лайки;
+
+3. Cсылка на github участников:
+Dimitri Void - https://github.com/leVOIDdeCOMPAGNIE
+
+4. Почта участников:
+Dimitri Void ultravoid@icloud.com
+
+**Githab page**
+- https://levoiddecompagnie.github.io/mesto-project/
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-export const cardSectionSelector = '.cards';
+export const cardSection = '.cards';
 
-export const profilePopupSection = '.popup_type_profile';
-export const cardPopupSection = '.popup_type_card-add';
-export const profileAvatarPopupSection = '.popup_type_profile-avatar';
-export const confirmDeletePopupSection = '.popup_type_confirm-delete';
-export const imagePopupSection = '.popup_type_picture';
+export const profilePopup = '.popup_type_profile';
+export const cardPopup = '.popup_type_card-add';
+export const profileAvatarPopup = '.popup_type_profile-avatar';
+export const confirmDeletePopup = '.popup_type_confirm-delete';
+export const imagePopup = '.popup_type_picture';
 
 export const profileTitle = document.querySelector('.profile__title');
 export const profileDescription = document.querySelector('.profile__description');
